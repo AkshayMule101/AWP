f131e5637e3e0929febf9b023070b97e8bc82e0f
AkshayMule101 <67921032+AkshayMule101@users.noreply.github.com>
2020-08-14 23:41:55 +0530

    Assignments

---
+++ Day10/1.js
@@ -0,0 +1,17 @@
+function readvalue() {
+  let a = document.querySelector("#inp1").value;
+  console.log(a);
+
+  let b = document.querySelector("#inp2").value
+  console.log(b);
+
+
+  let c = document.querySelector("#op1");
+
+  c.innerHTML = a + " " + b;
+
+  document.querySelector("#inp1").value = "";
+  document.querySelector("#inp2").value = "";
+
+
+}
